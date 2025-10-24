15c97bfc9413fc14c4a354dc7421a18052e08e37
njnadim2cse <njnadim2@gmail.com>
2025-10-24 18:31:19 +0600

    Delete controllers directory

--- controllers/dashboard.py
+++
@@ -1,9 +0,0 @@
-from odoo import http
-from odoo.http import request
-import json
-
-class EmployeePerformanceDashboard(http.Controller):
-
-    @http.route('/employee_performance/get_dashboard_data', type='json', auth='user')
-    def get_dashboard_data(self):
-        return request.env['employee.performance'].get_dashboard_data()
